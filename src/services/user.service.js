f033c70c99f7e2dc9428f54dc3526a1114b1cca4
meg9meg <magda110807@gmail.com>
2020-04-18 10:09:12 +0200

    update

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,10 +1,11 @@
-export default function authHeader() {
-    let user = JSON.parse(localStorage.getItem('user'));
-  
-    if (user && user.accessToken) {
-      return { Authorization: 'Bearer ' + user.accessToken };
-    } else {
-      return {};
-    }
+import axios from 'axios';
+import authHeader from './auth-header';
+
+const API_URL = 'http://localhost:8080/api/test/';
+
+class UserService {
+  getPublicContent() {
+    return axios.get(API_URL + 'all');
   }
-  
+
+  export default new UserService();
