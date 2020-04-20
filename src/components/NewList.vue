23d01558090082015580b50b29c8d377153dc354
meg9meg <magda110807@gmail.com>
2020-04-20 19:59:51 +0200

    update

--- src/components/NewList.vue
+++ src/components/NewList.vue
@@ -2,20 +2,15 @@
   <div class="col-md-12">
     <div class="card card-container">
         <form name="form">
-            <div id="app">
-                <div id="dfrom">
-                    <div class="center">
-                        <h1>Select start date</h1>
-                        <Datepicker></Datepicker>
+            <div class="form-row">
+                    <div class="form-group">
+                        <label for="dfrom">Start date</label>
+                        <b-form-datepicker v-model="list.dfrom" :min="min" :max="max" locale="en"></b-form-datepicker>
                     </div>
-                </div>
-                <div id="dto">
-                    <div class="center">
-                        <h1>Select end date</h1>
-                        <Datepicker></Datepicker>
-                    </div>
-                </div>
-            </div>
+						<div class="form-group">
+							<label for="dto">End date</label>
+							<b-form-datepicker v-model="list.dto" :min="min" :max="max" locale="en"></b-form-datepicker>
+						</div>
             <div class="form-group">
                 <label for="mountains"></label>
                 <input 
@@ -37,40 +32,35 @@
 
 
 <script>
-    import Datepicker from 'vuejs-datepicker'
 
-    export default {
-        name: 'dfrom',
-        components: {
-            Datepicker
-        }
-    }
-    export default {
-        name: 'dto',
-        components: {
-            Datepicker
-        }
-    }
+import List from '../models/list';
 
-new Vue({
-  el: "#app",
-  data: {
-  	start: null,
-    end: null,
-    diff: null
+export default {
+  name: 'List',
+  data() {
+    return {
+      list: new List('', '', ''),
+    };
   },
-  
-  mounted () {
-  	this.start = moment();
-    this.end = moment().add(7, 'days');
-    
-    let d = moment.duration(this.end.diff(this.start));
-    
-    this.diff = d.format()
-    
-    console.log(this.diff)
-  }
-})
+}
+
+/*export default { //?
+    data() {
+      const now = new Date()
+      const today = new Date(now.getFullYear(), now.getMonth(), now.getDate())
+      // from today
+      const minDate = new Date(today)
+      // to 1 month later
+      const maxDate = new Date(today)
+      maxDate.setMonth(maxDate.getMonth() + 1)
+      return {
+        value: '',
+        min: minDate,
+        max: maxDate
+      }
+    }
+  };
+  */
 
 </script>
 
