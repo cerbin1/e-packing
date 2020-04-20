03485afb8686839bb29bf3c8f7d93b002f323d1d
meg9meg <magda110807@gmail.com>
2020-04-20 20:36:41 +0200

    update

--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 //import Home from './views/Home.vue';
-import Login from './component/Login.vue';
-import Register from './component/Register.vue';
+import Login from './components/Login.vue';
+import Register from './components/Register.vue';
 
 Vue.use(Router);
 
